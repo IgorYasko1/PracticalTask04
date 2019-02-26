eadd616c1dac51007a54c4bf4353b7bd618265f5
IgorYasko1 <igoryasko121@yandex.ru>
2019-02-26 11:22:18 +0300

    add PracticalTask04 vowel

---
+++ vowel/VowelChecker.java
@@ -0,0 +1,21 @@
+package study.forthpracticaltask.vowel;
+
+public class VowelChecker {
+
+    private static final int[] VOWELSEQUENCE = {'A', 'E', 'I', 'O', 'U'};
+
+    public static boolean checkCharacter(final char a){
+        Character characterToUpperCase = Character.toUpperCase(a);
+        boolean flag = false;
+        for (int i = 0; i < VOWELSEQUENCE.length; i++){
+            if (VOWELSEQUENCE[i] == characterToUpperCase){
+                flag = true;
+                break;
+            }else {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+}
